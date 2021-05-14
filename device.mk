3cc206a3cc06490eb80780f7a573a92ce60f612e
luk1337 <priv.luk@gmail.com>
2021-05-14 11:46:34 +0530

    laurel_sprout: Drop android.hardware.thermal HAL
    * android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4.
      It should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- device.mk
+++ device.mk
@@ -457,12 +457,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.trinket
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
