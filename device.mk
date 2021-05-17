ea4dca25dbe3fd475f0ceb14274f9550520c35b7
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-05-17 12:20:51 +0000

    Add Nezuko overlays

--- device.mk
+++ device.mk
@@ -296,7 +296,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-nezuko
 
 PRODUCT_PACKAGES += \
     NotchBarKiller \
