abb8aae171dd006dcc285322dab983bec632516d
Cipher-oos <ma7moudmo7med628@outlook.sa>
2021-07-06 17:35:22 +0000

    EVO to Dot

--- dot_laurel_sprout.mk
+++ dot_laurel_sprout.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The Evolution-X Project
+# Copyright (C) 2021 The dot-X Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,16 +19,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common dot-X stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
 
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := evolution_laurel_sprout
+PRODUCT_NAME := dot_laurel_sprout
 PRODUCT_MODEL := Mi A3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -40,14 +44,20 @@ SKIP_ABI_CHECKS := true
 EXTRA_FOD_ANIMATIONS := true
 
 # Props
-EVO_MAINTAINER := HemantSachdeva
-EVO_BUILD_TYPE := OFFICIAL
-EVO_SUPPORT_URL := https://t.me/EvolutionXLaurel_Sprout
-EVO_DONATE_URL := https://pages.razorpay.com/HemantSachdevaa
-BUILD_USERNAME := Hemant
-BUILD_HOSTNAME := Evolution-X
+DOT_MAINTAINER := Mahmoud_Suleiman
+DOT_BUILD_TYPE := UNOFFICIAL
+DOT_SUPPORT_URL := https://t.me/Nezuko_A
+DOT_DONATE_URL := https://paypal.me/mmsuleiman
+BUILD_USERNAME := Cipher
+BUILD_HOSTNAME := dot
 
 WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_GAPPS_ARCH := arm64
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="laurel_sprout-user 10 QKQ1.190910.002 V11.0.15.0.QFQMIXM release-keys" \
+    PRODUCT_NAME="laurel_sprout"
